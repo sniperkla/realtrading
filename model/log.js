e18b8b3b757c916bb65237869269d9af9e9593d8
sniperkla <sniperkla@gmail.com>
2024-05-27 00:40:35 +0700

    newer2.1tp7

--- model/log.js
+++ model/log.js
@@ -9,16 +9,29 @@ const Log = new Schema(
     price: { type: String },
     takeProfit: { type: Object },
     time: { type: Date, default: Date.now },
+    binanceTakeProfitCp: { type: Object },
     binanceTakeProfit1: { type: Object },
     binanceTakeProfit2: { type: Object },
     binanceTakeProfit3: { type: Object },
     binanceTakeProfit4: { type: Object },
+    binanceTakeProfit5: { type: Object },
+    binanceTakeProfit6: { type: Object },
+    binanceTakeProfit7: { type: Object },
     binanceMarket: { type: Object },
     binanceStopLoss: { type: Object },
     takeProfitZone: { type: Object },
     lockStopLoss: {
       type: Object,
-      default: { lock: false, zone1: false, zone2: false, zone3: false }
+      default: {
+        lock: false,
+        zone1: false,
+        zone2: false,
+        zone3: false,
+        zone4: false,
+        zone5: false,
+        zone6: false,
+        zone7: false
+      }
     }
   },
   {
