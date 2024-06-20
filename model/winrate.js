f0fa0452fe8bd5b579bbb2b329b05c5194fd9305
sniperkla <sniperkla@gmail.com>
2024-06-20 18:20:50 +0700

    macd3.2mtg add monthlycheck

--- model/winrate.js
+++ model/winrate.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const WinRate = new Schema(
   {
+    _id: { type: String, default: 'winrate' },
     win: { type: Number, default: 0 },
     lose: { type: Number, default: 0 }
   },
