c278c2f07aa3a5096f6d883242504f7d5da4fb6e
sniperkla <sniperkla@gmail.com>
2024-10-08 20:20:33 +0700

    add highest martingale

--- app.js
+++ app.js
@@ -87,11 +87,12 @@ app.get(`/getbinance_${pathName}`, async (req, res) => {
       return item.previousMargin
     })
 
+    console.log('checkIntit', checkInit)
     const sum =
       previousMargin.reduce((sum, margin) => sum + margin, 0) || 'error'
 
     if (!checkInit) {
-      console.log("ur here jaa")
+      console.log('ur here jaa')
       initmarginmonthly.create({ _id: 'maxmartingale', highest: sum })
     } else {
       if (checkInit.highest < sum) {
