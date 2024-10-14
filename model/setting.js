9efe3dbfa9556d7ac024c2e6e6115efef81f5184
sniperkla <sniperkla@gmail.com>
2024-10-14 19:25:04 +0700

    add unpnl setting line bot

---
+++ model/setting.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Setting = new Schema(
+  {
+    _id: { type: String },
+    value: { type: Number },
+    status: { type: String }
+  },
+  {
+    timestamps: true
+  }
+)
+module.exports = mongoose.model('Setting', Setting)
